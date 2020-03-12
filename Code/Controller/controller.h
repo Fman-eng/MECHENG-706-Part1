162c3e431692c53c637da53905190ff8b31e4571
Freeman Porten <fpor552@aucklanduni.ac.nz>
2020-03-12 16:47:49 +1300

    Code: implimented PID class into controler class

--- Code/Controller/controller.h
+++ Code/Controller/controller.h
@@ -4,15 +4,22 @@
 */
 #ifndef Controller_h
 #define Controller_h
-
+#include "PID.h"
 #include "Arduino.h"
+#include <cstdlib>
 
 class Controller
 {
   private:
-    
+    PID* PIDVx;
+    PID* PIDVy;
+    PID* PIDW;
+    float[3] SaturateValues;
+    bool[3] isSaturated;
   public:
-    Controller();
-    float[3] WallFollow(float frontIR,float backIR);
+    Controller(PID* PIDVx, PID* PIDVy, PID* PIDW, float[3] saturateValues, bool[3] isSaturated);
+    float[3] WallFollow(float frontIR, float backIR, float targetDistance);
+    float[3] FrontDetect(float sonar, float targetDistance);
+    float[3] ApplyPID(float[3] in, float timeStep);
 };
 #endif
