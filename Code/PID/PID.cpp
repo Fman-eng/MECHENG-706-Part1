0a36e443277b21a5d36260d5932cdd0f06f819ee
Freeman <fpor552@aucklanduni.ac.nz>
2020-03-12 14:00:06 +1300

    Code: Updated PID class

--- Code/PID/PID.cpp
+++ Code/PID/PID.cpp
@@ -1,4 +1,4 @@
-#include PID.h
+#include "PID.h"
 
 PID::PID(float pGain, float iGain, float dGain){
   this->pGain = pGain;
@@ -12,7 +12,7 @@ float PID::GetControl(float in, float timeStep, bool isSaturated){
   float out;
   
   // Calculate integrator Error
-  if(!isSaturated){intergratorError += in*timeStep};
+  if(!isSaturated)intergratorError += in*timeStep;
   
   // Calculate PID control signal
   out =  pGain*in + iGain*intergratorError + dGain*(in-lastIn)/timeStep;
