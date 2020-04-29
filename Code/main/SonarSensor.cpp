7ab6feaa27848407790cd3fda2736689d4ce28cf
Jake Olliff <jakeolliff@gmail.com>
2020-04-29 14:49:19 +1200

    Commenting: All Sensor class code now commented

--- Code/main/SonarSensor.cpp
+++ Code/main/SonarSensor.cpp
@@ -1,10 +1,17 @@
 /*
   SonarSensor.cpp - A source file for the sonar sensor class for an arduino based robot.
   Group 2: Freeman Porten, Lachlan Barnes, Jake Olliff, Calvin Lee
+  
+  SonarSensor: a class that interfaces the physical sonar sensor with the rest of the arduino code.
+  It handles all of the calculations of distance so the user only needs to make one function call
+  to get a distance reading from the sonar sensor
 */
 #include "Arduino.h"
 #include "SonarSensor.h"
 
+//Constructor for the SonarSensor class, takes the arduino pin that trigger and echo
+//are attached to, saves them to a private class variable and sets the pin signal direction
+//of the pin on the arduino
 SonarSensor::SonarSensor(int triggerPin, int echoPin)
 {
     _triggerPin = triggerPin;
@@ -14,16 +21,22 @@ SonarSensor::SonarSensor(int triggerPin, int echoPin)
     pinMode(_echoPin, INPUT);
 }
 
+//getDistance function does not take any input arguments and returns the distance measured by the 
+//sonar sensor in mm
 float SonarSensor::getDistance()
 {
+  //Ensure trigger pin has been held low for at least 2 microseconds, then send 5 microsecond pulse
+  //in the trigger pin
     digitalWrite(_triggerPin, LOW);
     delayMicroseconds(2);
     digitalWrite(_triggerPin, HIGH);
     delayMicroseconds(5);
     digitalWrite(_triggerPin, LOW);
 
+    //Start timer and wait for the echo pin to go from high to low, timeout is set to 1m distance equivilent in microseconds
     float duration = pulseIn(_echoPin, HIGH, 5800);
 
+    //If timeout has occurred return 1 meter, else return the calculated distance
     if(duration == 0){
       return 1000;
     }else{
